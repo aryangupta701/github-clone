65bc0193518c54f052d37930ba6b6fe484f2fd7c
aryangupta701 <garyan447@gmail.com>
2021-08-24 15:53:06 +0530

    accessed avatar successfully

---
+++ main.js
@@ -0,0 +1,23 @@
+const button=document.getElementById('search')
+const searchbox=document.getElementById('text-input')
+
+button.addEventListener('click', ()=>{
+
+    const text=searchbox.value 
+    const xhr= new XMLHttpRequest()
+    const url=`https://api.github.com/users/${text}`
+    xhr.open('GET',url)
+   
+    xhr.onreadystatechange=()=>{ 
+        if(xhr.readyState==4 && xhr.status == 200)
+        {
+             
+           const profile=document.createElement('img')
+           const data= JSON.parse(xhr.responseText)
+           profile.setAttribute('src',data.avatar_url)
+           const cont=document.getElementById('image-con')
+           cont.appendChild(profile)
+        }
+    }
+    xhr.send()
+})
