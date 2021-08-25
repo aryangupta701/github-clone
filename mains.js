76a5305cfefa61d409eee5eb52cd725d808b03cd
aryangupta701 <garyan447@gmail.com>
2021-08-25 20:11:27 +0530

    extracted and displayed information from api

--- mains.js
+++ mains.js
@@ -11,14 +11,45 @@ button.addEventListener('click', ()=>{
     xhr.onreadystatechange=()=>{ 
         if(xhr.readyState==4 && xhr.status == 200)
         {
-             
+           const br=document.createElement('br')
+           const bro=document.createElement('br')
            const profile=document.createElement('img')
-        //    const hr=document.createElement('hr')
+           const follower=document.createElement('span')
+           const following=document.createElement('span')
+           const name=document.createElement('span')
+           const bio=document.createElement('p')
+           const hr=document.createElement('hr')
+           const hrt=document.createElement('hr')
+           const hrone=document.createElement('hr')
            const data= JSON.parse(xhr.responseText)
+
+            //data taking from api
+            //profile
            profile.setAttribute('src',data.avatar_url)
-           profile.setAttribute('style',"border-radius:50%; height: 100px ;display:block; margin-left:auto ; margin-right: auto ; ")
+           profile.setAttribute('style',"border-radius:50%; height: 100px ;margin-left:20px;  ")
+
+        //follows
+        follower.innerHTML=`${data.followers}`
+        following.innerHTML=`${data.following}`
+ 
+        //name & bio 
+        name.textContent=data.name
+        bio.textContent=data.bio
+
+        //first hr margin set
+        hrone.setAttribute('style',"margin-top:0px;")
            const cont=document.getElementById('data-display')
+           cont.appendChild(hrone)
            cont.appendChild(profile)
+           cont.appendChild(name)
+           cont.appendChild(hr)
+           cont.appendChild(bio)
+           cont.appendChild(hrt)
+           cont.appendChild(follower)
+           cont.appendChild(following)
+           cont.appendChild(br)
+           cont.appendChild(bro)
+
            
 
         }
